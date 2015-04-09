8a057c68798d8177c287b369fe3b00fa3b614f0c
cwahbong <cw.ahbong@gmail.com>
2015-04-09 15:15:52 +0800

    Refactor test classes.

--- tests/test_intergration.py
+++ tests/test_intergration.py
@@ -1,61 +1,13 @@
 """Integration tests for git-as.
 """
-import os
-import tempfile
-import unittest
 
 import sh
 
 
-class GitTest(unittest.TestCase):
-
-    """Test case that needs a git repository.
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        """Prepare a temporaty directory for repositories needed by test case.
-        """
-        cls._dir_root = tempfile.TemporaryDirectory(prefix=cls.__name__)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls._dir_root.cleanup()
-
-    def setUp(self):
-        self._old_cwd = os.getcwd()
-        self._dir_repo = tempfile.TemporaryDirectory(dir=self._dir_root.name)
-        sh.git.init(self._dir_repo.name)
-        os.chdir(self._dir_repo.name)
-
-    def tearDown(self):
-        os.chdir(self._old_cwd)
-        self._dir_repo.cleanup()
-
-    def assertGitConfigEqual(self, name, value):
-        """Checks that git local config name has that value."""
-        self.assertEqual(sh.git.config("--null", "--local", "--get", name), value + "\0")
-
-    def assertGitConfigEmpty(self, name):
-        """Checks that git local config name has no value."""
-        with self.assertRaises(sh.ErrorReturnCode_1, msg="Should be empty"):
-            sh.git.config("--null", "--local", "--get", name)
+from tests import base
 
 
-class FixedPreset(GitTest):
-    """Test case with predefined presets.
-
-    Subclass instances could set _preset by themselves.
-    """
-    _preset = {}
-
-    def setUp(self):
-        super().setUp()
-        for name, value in self._preset.items():
-            sh.git.config("--local", "as.preset.{}".format(name), value)
-
-
-class Simple(FixedPreset):
+class Simple(base.FixedPreset):
     """Test case with a preset with only one key-value.
     """
     _preset = {
@@ -79,7 +31,7 @@ class Simple(FixedPreset):
         self.assertGitConfigEmpty("section.abcd")
 
 
-class NoIntersection(FixedPreset):
+class NoIntersection(base.FixedPreset):
 
     _preset = {
         "first.section.name": "value",
